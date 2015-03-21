bc547971403c032ce3080cd51a65e9141bc9d7ac
Yashprit Singh <yashprits@gmail.com>
2015-03-22 01:09:45 +0530

    added first project

---
+++ eulkell_one.hs
@@ -0,0 +1,10 @@
+--problem_one : https://projecteuler.net/problem=1
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+-- The sum of these multiples is 23.
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+--main function is starting point of program
+main = print (sum [ x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0])
+-- print is function, that print output to console
+-- sum is function that add elements from array
+-- mod check number is modulues or not
